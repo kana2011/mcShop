3c7871611afe0bc20c918c4a47478ddbef0b5bef
Wasin Chintapanyakul <sentbysent@gmail.com>
2015-06-04 20:37:46 +0700

    Transactions update
    - add status
    - change id to txid
    - txid generate
    - getTransactions() update
    - etc.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
@@ -25,25 +26,55 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         return $info;
     }
 
-    public function makeTransaction($amount, $description) {
+    public function makeTransaction($amount, $description, $txid = NULL, $status = 1) { status: 0 = error 1 = success 2 = pending
         $transaction = new Transaction;
+
+        if($txid == NULL){ // generating txid
+            $txid = "";
+            $seed = str_split('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_#');
+            while(true){
+                shuffle($seed);
+                foreach (array_rand($seed, 9) as $k) $txid .= $seed[$k];
+                try{
+                    $t = Transactions::findorfail(array('txid',$txid));
+                }catch(ModelNotFoundException $e){
+                    break();
+                }
+            }
+        }
+
+        // insert transaction record
+        $transaction->txid = $txid;
         $transaction->user_id = $this->id;
         $transaction->amount = $amount;
         $transaction->description = $description;
+        $transaction->status = $status;
+        // process yeah
         $transaction->save();
     }
 
     public function getMoney() {
         $sum = 0;
-        $transactions = $this->getTransactions();
+        $transactions = $this->getTransactions(1);
         foreach($transactions as $transaction) {
             $sum = $sum + $transaction->amount;
         }
         return $sum;
     }
 
-	public function getTransactions() {
-		return Transactions::where('user_id', '=', $this->id)->orderBy('created_at', 'desc')->get()->all();
+    public function updateStatus($txid,$status = 1){
+        try{
+            $transaction = Transactions::findorfail($txid);
+        }catch(ModelNotFoundException $e){
+            return; //what I have to return?
+        }
+        $transaction->status = $status;
+        $transaction->save();
+    }
+
+	public function getTransactions($status = -1) {
+		if($status == -1) return Transactions::where('user_id', '=', $this->id)->orderBy('created_at', 'desc')->get()->all();
+        if($status >= 0 && $status <= 3) return Transactions::where('user_id', '=', $this->id)->where('status','=', $status)->orderBy('created_at', 'desc')->get()->all();
 	}
 
 }
