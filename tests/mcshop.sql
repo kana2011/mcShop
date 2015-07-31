6f1a3b005f8421bd22103f1a9762bb18321806fa
paphonbth <paphonb@icloud.com>
2015-07-31 08:26:38 +0700

    Updated test database

--- tests/mcshop.sql
+++ tests/mcshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2015 at 06:11 PM
+-- Generation Time: Jul 31, 2015 at 03:25 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -41,14 +41,15 @@ CREATE TABLE IF NOT EXISTS `authme` (
   `remember_token` varchar(255) NOT NULL,
   `money` int(11) NOT NULL DEFAULT '0',
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `authme`
 --
 
 INSERT INTO `authme` (`id`, `username`, `password`, `ip`, `lastlogin`, `x`, `y`, `z`, `world`, `email`, `isLogged`, `remember_token`, `money`, `updated_at`) VALUES
-(1, 'test', '$SHA$0475ce39fb153c35$0ea60ed41bf1985d6137d35278d6c9f9ea9345f948a1e416e0ad6a1da666be68', '198.18.0.1', 0, 0, 0, 0, 'world', 'your@email.com', 0, 'rfivcb6UXaAH9hEn2t6sf7Q7a4f7CawOO2j7bLG50zPX0WjLMvmxxb7oUSfa', 2000, '2015-06-03 22:44:21');
+(1, 'test', '$SHA$0475ce39fb153c35$0ea60ed41bf1985d6137d35278d6c9f9ea9345f948a1e416e0ad6a1da666be68', '198.18.0.1', 0, 0, 0, 0, 'world', 'your@email.com', 0, 'rfivcb6UXaAH9hEn2t6sf7Q7a4f7CawOO2j7bLG50zPX0WjLMvmxxb7oUSfa', 2000, '2015-06-03 22:44:21'),
+(2, 'test2', '$SHA$0475ce39fb153c35$0ea60ed41bf1985d6137d35278d6c9f9ea9345f948a1e416e0ad6a1da666be68', '198.18.0.1', 0, 0, 0, 0, 'world', 'your@email.com', 0, '', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -82,17 +83,20 @@ CREATE TABLE IF NOT EXISTS `shopitem` (
   `icomment` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
   `price` int(11) NOT NULL,
   `cmd` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `shopitem`
 --
 
 INSERT INTO `shopitem` (`id`, `dispname`, `igroup`, `icomment`, `price`, `cmd`) VALUES
-(1, 'TestItem', 1, NULL, 50, 'say Hello %player%'),
+(1, 'Test Item 1', 1, NULL, 50, 'say Hello %player%'),
 (2, 'ทดสอบไอเทม', 2, NULL, 50, NULL),
-(3, 'TestItem2', 1, NULL, 100, NULL),
-(4, 'ทดสอบไอเทม2', 2, NULL, 100, NULL);
+(3, 'Test Item 2', 1, NULL, 100, NULL),
+(4, 'ทดสอบไอเทม2', 2, NULL, 100, NULL),
+(5, 'Test Item 3', 1, NULL, 150, NULL),
+(6, 'ทดสอบไอเทม3', 2, NULL, 150, NULL),
+(7, 'Test Item 4', 1, NULL, 200, NULL);
 
 -- --------------------------------------------------------
 
@@ -113,10 +117,14 @@ CREATE TABLE IF NOT EXISTS `tokens` (
 --
 
 INSERT INTO `tokens` (`id`, `userid`, `appname`, `created_at`, `updated_at`) VALUES
-('AeL9ubhmycRtpNS', 1, 'Android', '2015-07-23 15:13:54', '2015-07-23 15:13:54'),
-('b83YC0ZlyIameWP', 1, 'Android', '2015-07-24 13:45:08', '2015-07-24 13:45:08'),
-('OR1oiTLEMkHgC4A', 1, 'Android', '2015-07-23 15:21:50', '2015-07-23 15:21:50'),
-('vEjQBARXe6lUCSh', 1, 'Android', '2015-07-24 06:08:10', '2015-07-24 06:08:10');
+('ajLlHKdx92ynprv', 2, 'Android', '2015-07-25 13:26:27', '2015-07-25 13:26:27'),
+('c1TVwzxlWvAqmLh', 2, 'Android', '2015-07-25 10:50:36', '2015-07-25 10:50:36'),
+('LrCsIgHwXRqb06z', 1, 'Android', '2015-07-30 13:09:48', '2015-07-30 13:09:48'),
+('TVd2oMxBrmYtP7z', 1, 'Android', '2015-07-25 09:43:03', '2015-07-25 09:43:03'),
+('VcWI63uXKlLqFgQ', 1, 'Android', '2015-07-25 09:03:17', '2015-07-25 09:03:17'),
+('VGCbOLcyE7BPN8d', 1, 'Android', '2015-07-25 13:26:03', '2015-07-25 13:26:03'),
+('xoLjClu8XvMnWiS', 1, 'Android', '2015-07-28 13:45:58', '2015-07-28 13:45:58'),
+('XWyECwmF41ZGH0k', 1, 'Android', '2015-07-27 13:14:21', '2015-07-27 13:14:21');
 
 -- --------------------------------------------------------
 
@@ -193,7 +201,7 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT for table `authme`
 --
 ALTER TABLE `authme`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `shopgroup`
 --
@@ -203,7 +211,7 @@ ALTER TABLE `shopgroup`
 -- AUTO_INCREMENT for table `shopitem`
 --
 ALTER TABLE `shopitem`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `transactions`
 --
