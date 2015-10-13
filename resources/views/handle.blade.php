b91602381162f1064acdfc67dc704ef06aaecba1
sent44 <sentbysent@gmail.com>
2015-10-13 19:26:12 +0700

    Fix handle
    - remove jquery redirect
    - change from local jquery to remote jquery

--- resources/views/handle.blade.php
+++ resources/views/handle.blade.php
@@ -1,14 +1,12 @@
 <html>
 <head>
-<script type="text/javascript" src="assets/components/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/mgalante/jquery.redirect/master/jquery.redirect.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 </head>
 <body>
 <form style="display: hidden" action="/" method="POST" id="form">
 <input type="hidden" id="var1" name="path" value=""/>
 <input type="hidden" id="var2" name="statusCode" value=""/>
 <input type="hidden" id="var3" name="reason" value=""/>
-
 <script type="text/javascript">
 	$("#var1").val("<?PHP echo $path; ?>");
 	$("#var2").val("<?PHP echo $statusCode; ?>");
